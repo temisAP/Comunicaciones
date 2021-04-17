f3a54d218798257a54f3a1c0ae924fd26ba206cb
temisAP <74715759+temisAP@users.noreply.github.com>
2021-04-17 16:21:04 +0200

    Times

--- Extraer_Data.m
+++ Extraer_Data.m
@@ -2,8 +2,6 @@ clc
 clear all
 close all
 
-
-
 %% LEER DATOS
 
 filename = 'Elevation.xlsx';
@@ -16,12 +14,57 @@ names = {'10 deg', '20 deg', '30 deg'};
 Elevation = struct();
 
 for s = 1:length(sheets)
-    data = xlsread(filename,sheets(s));
+    [data,txtdata] = xlsread(filename,sheets(s));
     
     Elevation(s).Name = names{s};
-    %Elevation(s).t = data(:,1); % No se como hacer para que coja esta
-    %columna
+    Elevation(s).t = datetime(txtdata(2:end,1),'InputFormat','dd MMM yyyy HH:mm:ss,SSS'); 
     Elevation(s).azimuth = data(:,1);
     Elevation(s).elevation = data(:,2);
-    Elevation(s).range = data(:,3);
+    Elevation(s).range = data(:,3);   
+    
+end
+
+%% PLOT
+
+rep = 'n';
+if rep == 'y'
+    
+    % All
+    figure(1)
+    hold on
+    for s = 1:length(sheets)
+        plot(Elevation(s).t , Elevation(s).azimuth,  'DisplayName', ['Azimuth ', Elevation(s).Name])
+        plot(Elevation(s).t , Elevation(s).elevation,  'DisplayName', ['Elevation ', Elevation(s).Name])
+        plot(Elevation(s).t , Elevation(s).range,  'DisplayName', ['Range ', Elevation(s).Name])           
+    end
+    legend('Location','bestoutside')
+    grid on; box on;
+    
+    % Azimuth
+    figure(2)
+    hold on
+    for s = 1:length(sheets)
+        plot(Elevation(s).t , Elevation(s).azimuth,  'DisplayName', ['Azimuth ', Elevation(s).Name])
+    end
+    legend('Location','bestoutside')
+    grid on; box on;
+    
+    %Elevation
+    figure(3)
+    hold on
+    for s = 1:length(sheets)
+       plot(Elevation(s).t , Elevation(s).elevation,  'DisplayName', ['Elevation ', Elevation(s).Name])
+    end
+    legend('Location','bestoutside')
+    grid on; box on;
+    
+    % Range
+    figure(4)
+    hold on
+    for s = 1:length(sheets)
+       plot(Elevation(s).t , Elevation(s).range,  'DisplayName', ['Range ', Elevation(s).Name])           
+    end
+    legend('Location','bestoutside')
+    grid on; box on;
+    
 end
