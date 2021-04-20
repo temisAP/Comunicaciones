579e9d8ca0605fa2db220a75ce2b71817f7b8e22
Danieldelriovelilla <daniel_drv@hotmail.es>
2021-04-20 23:20:42 +0200

    plot de los cojones

--- Matlab_Code/Data/Task4.m
+++ Matlab_Code/Data/Task4.m
@@ -6,16 +6,20 @@ close all
 load('MODCOD.mat')
 load('Pase_CN.mat')
 
-CN = 10*sin(linspace(0,pi,101))+2;
+CN = Pase(1).CN{Pase(1).max_idx};
+idx = zeros(size(CN,1));
+%mod = zeros(length(idx),1);
 
-idx = zeros(length(modcod),size(CN,2));
-mod = zeros(size(CN));
-
+    idx = [modcod(1,1) <= CN]';
+    mod(idx==1) = modcod(1,1);
 for i = 2:length(modcod)-1
     
-    idx(i,:) = modcod(i,2) < CN & modcod(i+1,2) >= CN;
-    mod(idx(i,:)==1) = modcod(i,1);
-    
-    
+    idx = [modcod(i,1) <= CN & modcod(i-1,1) >= CN]';
+    mod(idx==1) = modcod(i,1);
     
 end
+
+figure()
+    hold on
+    plot(Pase(1).t{Pase(1).max_idx}, mod)
+    plot(Pase(1).t{Pase(1).max_idx}, Pase(1).CN{Pase(1).max_idx})
