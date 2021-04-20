b512157206b707752efd366302957a7893cc7af6
anabelsoria <anabelsoriacarro@gmail.com>
2021-04-20 12:28:12 +0200

    access

--- Matlab_Code/Data/Extraer_Data.m
+++ Matlab_Code/Data/Extraer_Data.m
@@ -83,3 +83,31 @@ if rep == 'y'
     grid on; box on;
     
 end
+
+%% ACCESOS
+
+filename = 'Access.xlsx';
+xlRange = 'A2:D2';
+sheets = [1,2,3];
+names = {'10 deg', '20 deg', '30 deg'};
+angulo = [10, 20, 30];
+
+for s = 1:length(sheets)
+    [data,txtdata] = xlsread(filename,sheets(s));
+    
+    Access(s).Name = names{s};
+    Access(s).start = datetime(txtdata(2:end,1),'InputFormat','dd MMM yyyy HH:mm:ss,SSS'); 
+    Access(s).end = datetime(txtdata(3:end,1),'InputFormat','dd MMM yyyy HH:mm:ss,SSS'); 
+    Access(s).num = data(:,1);
+    Access(s).duration = data(:,4)/60;
+    Access(s).average = 0;
+    
+    % Average duration
+    for i = 1:length(Access(s).num)
+        Access(s).average = Access(s).average + Access(s).duration(i);
+    end
+    Access(s).average = Access(s).average/length(Access(s).duration);
+    
+end
+
+save('Access.mat', 'Access')
