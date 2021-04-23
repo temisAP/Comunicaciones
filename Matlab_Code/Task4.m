56f8126f361db2cebf06bc007559437b51741403
temisAP <74715759+temisAP@users.noreply.github.com>
2021-04-23 16:53:30 +0200

    push

--- Matlab_Code/Task4.m
+++ Matlab_Code/Task4.m
@@ -64,63 +64,82 @@ for a = 1:length(Pase)
             %%% Bajada
             if CN(t) < CN(t-1)
                 
+                pasos_bajada = pasos_bajada + 1;
                 
-                if  round(tlastchoose) == t_choose
-                    
-                    pasos_bajada = pasos_bajada + 1;
-                    
-                    if round(tlastmod) >= tmod
-                        % Si las tres siguientes no dan problema
-                        pos = find(modcod(:,2) <= CN(t) == 1);
-                        
-                        % Si no hay que cambiar la modulación en los
-                        % siguientes tres pasos se mantiene 
-                        if modcod(pos(1),2) == Pase(a).MC{p}(t-1) &&...
-                                Pase(a).MC{p}(t-1) <= Pase(a).CN{p}(t+t_choose-1) &&...
-                                Pase(a).MC{p}(t-1) <= Pase(a).CN{p}(t+2*t_choose-1) && ...
-                                Pase(a).MC{p}(t-1) <= Pase(a).CN{p}(t+3*t_choose-1)
-                            Pase(a).MC{p}(t:t+3*t_choose-1) = Pase(a).MC{p}(t-1);
-                            Pase(a).MCef{p}(t:t+3*t_choose-1) = Pase(a).MCef{p}(t-1);                
-                            t = t+3*t_choose-1;
-                        else
-                            Pase(a).MC{p}(t) = modcod(pos(1),2);
-                            Pase(a).MCef{p}(t) = modcod(pos(1),3);
-                            tlastmod = 0;
-                        end
-                        
-                    elseif round(tlastmod) < tmod
-                        if modcod(pos(1),2) == Pase(a).MC{p}(t-1) &&...
-                                Pase(a).MC{p}(t-1) <= Pase(a).CN{p}(t+t_choose-1) &&...
-                                Pase(a).MC{p}(t-1) <= Pase(a).CN{p}(t+2*t_choose-1) && ...
-                                Pase(a).MC{p}(t-1) <= Pase(a).CN{p}(t+3*t_choose-1)
-                            Pase(a).MC{p}(t) = Pase(a).MC{p}(t-1);
-                            Pase(a).MCef{p}(t) = Pase(a).MCef{p}(t-1);
-                            
-                        end
+                if sw_salto == 0 
+                    % Salto si llega al limite de modulacion
+                    if length(tpase) - t < t_mod
+                        dT = length(tpase) - t;
+                    else
+                        dT = t_mod;
                     end
+                    tlastmod = 0;
+                    pos = find(modcod(:,2) <= CN(t+dT) == 1);
+                    Pase(a).MC{p}(t) = modcod(pos(1),2);
+                    Pase(a).MCef{p}(t:t) = modcod(pos(1),3);
+                    nextmod = modcod(pos(2),2);
+                    nexteff = modcod(pos(2),3);
+                    sw_salto = 1;
+                                       
+                elseif sw_salto == 1 && round(tlastmod) < dT
+                    % han pasado 30 s y todavia puede transmitir con esa mod
+                    Pase(a).MC{p}(t) = Pase(a).MC{p}(t-1);
+                    Pase(a).MCef{p}(t) = Pase(a).MCef{p}(t-1);
+                    
+                elseif round(tlastmod) >= dT && round(tlastchoose) == t_choose 
+                    % Salta a la siguiente modulacion
+                    pos = find(modcod(:,2) <= CN(t) == 1);
+                    if modcod(pos(1),2) == Pase(a).MC{p}(t-1)
+                        Pase(a).MC{p}(t) = Pase(a).MC{p}(t-1);
+                        Pase(a).MCef{p}(t) = Pase(a).MCef{p}(t-1);
+                    else
+                        Pase(a).MC{p}(t) = modcod(pos(1),2);
+                        Pase(a).MCef{p}(t) = modcod(pos(1),3);
+                        % Si en el paso anterior se había pasado hay que
+                        % reescribir
+                        if Pase(a).MC{p}(t-1) > Pase(a).CN{p}(t-1)
+                            
+                            n_escalones = 1;
+                            if Pase(a).MC{p}(t-1) == Pase(a).CN{p}(t-1-t_choose)
+                                n_escalones = 2;
+                            end
+                            if Pase(a).MC{p}(t-1) == Pase(a).CN{p}(t-1-2*t_choose)
+                                n_escalones = 3;
+                            end
+                        
+                            Pase(a).MC{p}(t-n_escalones*t_choose:t) = Pase(a).MC{p}(t);
+                            Pase(a).MCef{p}(t-n_escalones*t_choose:t) = Pase(a).MCef{p}(t);
+                        end
+                        sw_salto = 0;
+                    end
+                elseif round(tlastmod) >= dT && round(tlastchoose) ~= t_choose
+                    % han pasado 30 s pero todavia no se puede cambiar 
+                    Pase(a).MC{p}(t) = Pase(a).MC{p}(t-1);
+                    Pase(a).MCef{p}(t) = Pase(a).MCef{p}(t-1);
+                    
                 end
-                
-                if round(tlastchoose) == t_choose
-                    tlastchoose = 0;
-                end
-                
+        
             end
             
+            if round(tlastchoose) == t_choose
+                tlastchoose = 0;
+            end
             
         end
+        
     end
 end
-    
-    for a = 1:1
-        for p = 1:3
-            figure()
-            hold on
-            plot(Pase(a).t{p}, Pase(a).MC{p}, 'DisplayName', 'Modulacion')
-            plot(Pase(a).t{p}, Pase(a).MCef{p}, 'DisplayName', 'Eficiencia')
-            plot(Pase(a).t{p}, Pase(a).CN{p}, 'DisplayName', 'C/N')
-            legend()
-            title(['Angulo ' num2str(a) ' Pase ' num2str(p)])
-        end
+
+for a = 1:1
+    for p = 1:3
+        figure()
+        hold on
+        plot(Pase(a).t{p}, Pase(a).MC{p}, 'DisplayName', 'Modulacion')
+        plot(Pase(a).t{p}, Pase(a).MCef{p}, 'DisplayName', 'Eficiencia')
+        plot(Pase(a).t{p}, Pase(a).CN{p}, 'DisplayName', 'C/N')
+        legend()
+        title(['Angulo ' num2str(a) ' Pase ' num2str(p)])
     end
-    
-    save('Data/Pase_CN_1s.mat', 'Pase')
+end
+
+save('Data/Pase_CN_1s.mat', 'Pase')
